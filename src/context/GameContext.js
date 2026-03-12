4c65e3af4f2dde93abfb4e1a9092da7f78943389
liabra <lia.atake@gmail.com>
2026-03-12 14:45:56 +0000

    fix GameContext content

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,44 +1,17 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { GameProvider } from './context/GameContext';
-import GamesPage from './pages/GamesPage';
-import BattleGame from './pages/games/BattleGame';
-import BingoGame from './pages/games/BingoGame';
-import EscapeGame from './pages/games/EscapeGame';
-import MemoryGame from './pages/games/MemoryGame';
-import SecretKeyGame from './pages/games/SecretKeyGame';
-import SpeedrunGame from './pages/games/SpeedrunGame';
-import UpdatesPage from './components/UpdatesPage';
-import LoginForm from './LoginForm';
-import ProfileForm from './ProfileForm';
-import ResultsPage from './ResultsPage';
-import SessionComponent from './SessionComponent';
-import UserList from './UserList';
-import './App.css';
+import React, { createContext, useContext, useState } from 'react';
 
-function App() {
+const GameContext = createContext();
+
+export function GameProvider({ children }) {
+  const [xp, setXp] = useState(0);
+
   return (
-    <GameProvider>
-      <div className="app-wrapper">
-        <Routes>
-          <Route path="/" element={<GamesPage />} />
-          <Route path="/games" element={<GamesPage />} />
-          <Route path="/games/battle" element={<BattleGame />} />
-          <Route path="/games/bingo" element={<BingoGame />} />
-          <Route path="/games/escape" element={<EscapeGame />} />
-          <Route path="/games/memory" element={<MemoryGame />} />
-          <Route path="/games/secret-key" element={<SecretKeyGame />} />
-          <Route path="/games/speedrun" element={<SpeedrunGame />} />
-          <Route path="/updates" element={<UpdatesPage />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/profil" element={<ProfileForm />} />
-          <Route path="/resultats" element={<ResultsPage />} />
-          <Route path="/session" element={<SessionComponent />} />
-          <Route path="/users" element={<UserList />} />
-        </Routes>
-      </div>
-    </GameProvider>
+    <GameContext.Provider value={{ xp, setXp }}>
+      {children}
+    </GameContext.Provider>
   );
 }
 
-export default App;
+export function useGame() {
+  return useContext(GameContext);
+}
