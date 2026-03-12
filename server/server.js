53837b6beb8b1f5c81d63b7829146d2e28d66e9b
liabra <lia.atake@gmail.com>
2026-03-12 15:31:17 +0000

    fix railway start script and serve react build

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-// server/server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 const port = process.env.PORT || 3000;
-const mySecret = process.env['MONGO_URI'];
+const mySecret = process.env.MONGO_URI;
 
 mongoose.connect(mySecret, {
   useNewUrlParser: true,
@@ -23,10 +24,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', userRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// Servir le frontend React buildé
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
