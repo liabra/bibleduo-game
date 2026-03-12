acff713d2000abd9117e06f6d7be71df002d211e
liabra <lia.atake@gmail.com>
2026-03-12 20:37:52 +0000

    test

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
-const sessionRoutes = require('./routes/sessionRoutes');
+const userRoutes = require('./server/routes/userRoutes');
+const sessionRoutes = require('./server/routes/sessionRoutes');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const mySecret = process.env['MONGO_URI'];
+const mySecret = process.env.MONGO_URI;
 
-// Set strictQuery to false to adopt the upcoming default behavior in Mongoose 7
 mongoose.set('strictQuery', false);
 
-// Middleware to parse JSON
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose.connect(mySecret, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error('Error connecting to MongoDB:', err);
-});
+mongoose.connect(mySecret)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-// Define your /api/test route
 app.get('/api/test', (req, res) => {
   res.send('API works');
 });
 
-// Mount routes
 app.use('/api', userRoutes);
 app.use('/api', sessionRoutes);
 
-// Start the server
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
